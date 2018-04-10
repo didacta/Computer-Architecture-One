f506c8d740d0ac52a1c7069522faf294bdb0dbe1
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-04-10 16:09:52 -0400

    Dynamic File Load
    node ls8 print8.ls8 or node ls8 mult.ls8 will load the proper code into program for cpu.poke.

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,11 +1,18 @@
 const RAM = require('./ram');
 const CPU = require('./cpu');
+const fs = require('fs');
 
 /**
  * Load an LS8 program into memory
  *
  * TODO: load this from a file on disk instead of having it hardcoded
  */
+const filename = process.argv[2] + '';
+const file = fs.readFileSync(filename, 'binary').split('\n');
+const cleanFile = file.map(line => {
+    return line.replace(/\#.*/,'');
+});
+
 function loadMemory() {
 
     // Hardcoded program to print the number 8 on the console
@@ -21,20 +28,25 @@ function loadMemory() {
     // ];
 
     //mult.ls8
-    const program = [
-        '10011001',
-        '00000000',
-        '00001000',
-        '10011001',
-        '00000001',
-        '00001001',
-        '10101010',
-        '00000000',
-        '00000001',
-        '01000011',
-        '00000000',
-        '00000001',
-    ]
+    // const program = [
+    //     '10011001',
+    //     '00000000',
+    //     '00001000',
+    //     '10011001',
+    //     '00000001',
+    //     '00001001',
+    //     '10101010',
+    //     '00000000',
+    //     '00000001',
+    //     '01000011',
+    //     '00000000',
+    //     '00000001',
+    // ]
+    let program = [];
+    cleanFile.forEach(line => {
+        if (line === '') return;
+        program.push(line);
+    });
 
     // Load the program into the CPU's memory a byte at a time
     for (let i = 0; i < program.length; i++) {
